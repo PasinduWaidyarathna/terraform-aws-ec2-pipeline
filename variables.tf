716ad5fd00c7f1a5e0329296f412986d24a96bf0
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-04-23 16:37:31 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-05572e392e80aee89"
+}
